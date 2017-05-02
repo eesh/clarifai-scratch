5a867d21f4e26eabb7cd90693c704691e3ff464b
Eesh Chilukalapalli <eeshlikhith@gmail.com>
2017-05-02 10:33:01 -0400

    support to search via URL vs databytes

--- clarifai.js
+++ clarifai.js
@@ -2,6 +2,7 @@
 
     var clarifai = undefined;
     var clarifaiLoaded = false;
+    var predictionResults = [];
 
     $.getScript('https://eesh.github.io/scratch-test/clarifai.js', checkClarifai);
 
@@ -16,12 +17,20 @@
 
     function initialiseClarifai(apikey1, apikey2) {
       var clarifai = new Clarifai.App(apikey1, apikey2);
-
+      if (clarifai == undefined) {
+        clarifaiLoaded = false;
+      } else {
+        clarifaiLoaded = true;
+      }
     }
 
-    function performSearch(image) {
+    function performSearch(image, callback) {
       if(image == undefined) {
         image = null; //getImageFromCamera
+      } else {
+        if (image.substring(0,4) != "http") {
+          image = { base64 : image };
+        }
       }
       if(clarifaiLoaded == false) {
         console.log("Clarifai not loaded");
@@ -29,24 +38,38 @@
       }
       clarifai.models.predict(Clarifai.GENERAL_MODEL, image).then(
         function(response) {
-          console.log(response);
+          if(response.status.code == 10000) {
+            processResponse(response);
+          } else {
+            console.log(response);
+          }
         },
         function(err) {
           console.error(err);
         }
       );
+
+      function processResponse(response) {
+        response.outputs.data.concepts.forEach(function(result) {
+          predictionResults.push(result.name);
+        });
+      }
     }
 
     function getResultsLength() {
-
+      return predictionResults.length;
     }
 
     function getItemFromResults(index) {
-
+      if(index >= 0 && index < predictionResults.length) {
+        return predictionResults[index];
+      } else {
+        console.log("Index out of bounds");
+      }
     }
 
     function clearResults() {
-
+      predictionResults = [];
     }
 
     // Cleanup function when the extension is unloaded
@@ -55,14 +78,23 @@
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
+      if (clarifaiLoaded == true) {
         return {status: 2, msg: 'Ready'};
+      }
+      return {status: 1, msg: 'Not connected to API'};
+    };
+
+    ext._shutdown = function() {
+      predictionResults = [];
+      clarifaiLoaded = false;
+      clarifai = undefined;
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
           ['w', 'Connect to API: %s %s', 'initializeClarifai', 'vKCXoGNBI9RrFYs33BUxcDOB3WoMJ5rK9D0hSD4J', 'cva5xoSvMf_htwZZHIZ_9JhjThL8N0BX_PqaJPUj'],
-          ['w', 'Search image', 'performSearch'],
+          ['W', 'Search image %s', 'performSearch'],
           ['r', 'Image results count', 'getResultsLength'],
           ['r', 'Get result %n from results', 'getItemFromResults'],
           ['r', 'Clear results', 'clearResults']
@@ -70,5 +102,5 @@
     };
 
     // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
+    ScratchExtensions.register('Clarifai extension', descriptor, ext);
 })({});
