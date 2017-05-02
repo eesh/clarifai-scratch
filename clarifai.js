4182e2e1a00463504261751b6849c83060ddbcb9
Eesh Chilukalapalli <eeshlikhith@gmail.com>
2017-05-02 11:50:29 -0400

    camera support

--- clarifai.js
+++ clarifai.js
@@ -4,6 +4,10 @@
     var clarifaiLoaded = false;
     var predictionResults = [];
 
+
+    var videoElement = undefined;
+    var hidden_canvas = undefined;
+
     $.getScript('https://eesh.github.io/scratch-test/clarifai.js', checkClarifai);
 
     function checkClarifai() {
@@ -15,18 +19,77 @@
       }
     }
 
-    function initialiseClarifai(apikey1, apikey2) {
+    function initializeCamera() {
+      videoElement = document.createElement('video');
+      videoElement.id = "camera-stream";
+      hidden_canvas = document.createElement('canvas');
+      hidden_canvas.id = "imageCanvas"
+      navigator.getUserMedia(
+          // Options
+          {
+              video: true
+          },
+          // Success Callback
+          function(stream){
+
+              // Create an object URL for the video stream and
+              // set it as src of our HTLM video element.
+              videoElement.src = window.URL.createObjectURL(stream);
+
+              // Play the video element to show the stream to the user.
+              videoElement.play();
+
+          },
+          // Error Callback
+          function(err){
+
+              // Most common errors are PermissionDenied and DevicesNotFound.
+              console.error(err);
+
+          }
+      );
+    }
+
+    function takeSnapshot(){
+
+        var hidden_canvas = document.querySelector('#imageCanvas'),
+        video = document.querySelector('#camera_stream');
+
+        // Get the exact size of the video element.
+        width = video.videoWidth,
+        height = video.videoHeight,
+
+        // Context object for working with the canvas.
+        context = hidden_canvas.getContext('2d');
+
+
+        // Set the canvas to the same dimensions as the video.
+        hidden_canvas.width = width;
+        hidden_canvas.height = height;
+
+        // Draw a copy of the current frame from the video on the canvas.
+        context.drawImage(video, 0, 0, width, height);
+
+        // Get an image dataURL from the canvas.
+        var imageDataURL = hidden_canvas.toDataURL('image/png');
+        return imageDataURL;
+    }
+
+    function initializeClarifai(apikey1, apikey2) {
       var clarifai = new Clarifai.App(apikey1, apikey2);
       if (clarifai == undefined) {
         clarifaiLoaded = false;
       } else {
         clarifaiLoaded = true;
+        initializeCamera();
       }
     }
 
     function performSearch(image, callback) {
       if(image == undefined) {
-        image = null; //getImageFromCamera
+        if(imageCanvas != undefined) {
+          image = takeSnapshot();
+        } else callback();
       } else {
         if (image.substring(0,4) != "http") {
           image = { base64 : image };
